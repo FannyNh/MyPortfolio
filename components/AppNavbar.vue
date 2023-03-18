dbc0e31ae8af7a222950e36133669396635a14d0
FannyNh <thethirdmoira@gmail.com>
2023-03-18 15:15:04 +0100

    ui

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="w-full h-16 absolute">
-      <div class="w-full h-full absolute bg-slate-100 opacity-10"></div>
+    <div class="w-full h-16 fixed z-50">
+      <div class="w-full h-full absolute bg-slate-100 opacity-50"></div>
       <div class="container h-full mx-auto text-violet-600 flex justify-between items-center">
         <app-logo></app-logo>
         <app-links class="md:block hidden"></app-links>
